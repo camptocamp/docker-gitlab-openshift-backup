92881d3cc9061654fe8b0b8d3a14ffc9163ea740
Max Laager <max.laager@camptocamp.com>
2019-03-28 15:11:26 +0100

    script streamline

--- gitlab-backup.sh
+++ gitlab-backup.sh
@@ -55,36 +55,37 @@ if [ -z "$SRE_TEAM" ]; then
  echo "please define sre team paramete example -s=sret1"
 fi
 
-POD=$(oc get pod  -o jsonpath='{.items.*.metadata.name}' | sed 's/ /\n/g' | grep 'gitlab-task-runner-') || exit 0 
-OUTPUT=$(oc exec $POD -i "backup-utility") || exit 0
+export POD=$(oc get pod  -o jsonpath='{.items.*.metadata.name}' | sed 's/ /\n/g' | grep 'gitlab-task-runner-') || exit 0 
+export OUTPUT=$(oc exec $POD -i "backup-utility") || exit 0
 if [ -z "$PROMETHEUS_PUSHGATEWAY_URL" ]; then
     echo "$OUTPUT" || exit 0
     exit 0
 else
-    RESULT=$?
-    H=$(oc exec $POD -i "/bin/echo" "\$HOSTNAME") || exit 0
-    OS=$(oc exec $POD -i "/bin/echo" "\$OSTYPE") || exit 0
-    METRICS=$(
+    export RESULT=$?
+    export H=$(oc exec $POD -i "hostname") || exit 0
+    export OS=$(oc exec $POD -it "cat" "/etc/os-release" ) || exit 0
+    export OS=$(echo "$OS" | grep -e "^NAME=" | sed 's/NAME=//g' | sed 's/"//g')
+    export METRICS=$(
         #define the type of metric 
         #for every repo we have a metric indicating backup success
         echo "#TYPE gitlab_backup_repo gauge";
         echo "$OUTPUT" | 
         # awk is your friend to transform the output into prometheus metrics
-        awk -v h="$H" -v ostype="$OS" -v sre_team="$SRE_TEAM"  -v line="$LINE" '/ .*\[.*\]/{
+        awk -v h="$POD" -v ostype="$OS" -v sre_team="$SRE_TEAM"  -v line="$LINE" '/ .*\[.*\]/{
         gsub("Dumping","");
-        gsub("\[DONE\]",0); 
-        gsub("\[SKIPPED\]",1);
-        gsub("\[WARNING\]",2);
-        gsub("\[ERROR\]",3);
-        gsub("\[.*\]",4);
-        gsub(" \* ","");
+        gsub("\\\[DONE\\\]",0); 
+        gsub("\\\[SKIPPED\\\]",1);
+        gsub("\\\[WARNING\\\]",2);
+        gsub("\\\[ERROR\\\]",3);
+        gsub("\\\[.*\\\]",4);
+        gsub(" \\\* ","");
         gsub("/","-");
         gsub("-","_");
         printf "gitlab_backup_repo {repo=\"%s\",certname=\"%s\",os=\"%s\",project=\"gitlab_backup\",line=\"%s\",sre_team=\"%s\"} %d\n",$1,h,ostype,line,sre_team, $NF;
         }'; 
         # finaly the global sucess of the backup
-        echo -e "#TYPE gitlab_backup_success gauge\n
-        gitlab_backup_success{certname=\"$H\",os=\"$OS\", project=\"gitlab_backup\",line=\"$LINE\",sre_team=\"$SRE_TEAM\"} $RESULT\n") || exit 0
+        echo -e "#TYPE gitlab_backup_success gauge"
+        echo -e "gitlab_backup_success{certname=\"$H\",os=\"$OS\", project=\"gitlab_backup\",line=\"$LINE\",sre_team=\"$SRE_TEAM\"} $RESULT") || exit 0
 
     # lets push the metrics to the gateway
     echo -e "$METRICS" | curl --data-binary @- $PROMETHEUS_PUSHGATEWAY_URL || exit 0
