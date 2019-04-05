f058b1cc204b327c7a13ab36965f35a2b5447d2a
Julien Godin <julien.godin@camptocamp.com>
2019-04-05 15:14:15 +0200

    Add SKIP parameters

--- gitlab-backup.sh
+++ gitlab-backup.sh
@@ -1,12 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
 
 ###################################################################
 #Script Name    : finds the gitlab task runner, intiate a backup
 #                 and report metrics to a prometheus pushgateway
-#Description    :                                                                                 
-#Args           :                                                                                           
-#Author         : Max Laager                                                
-#Email          : max.laager@camptocamp.com                                           
+#Description    :
+#Args           :
+#Author         : Max Laager
+#Email          : max.laager@camptocamp.com
 ###################################################################
 
 function usage()
@@ -18,6 +18,7 @@ function usage()
     echo "\t-p(or --prometheus_pushgateway_url)=prometheus_pushgateway_url (reports metrics backup)"
     echo "\-s(or --sre_team)=sre_team (sret1 or sret2, etc..)"
     echo "\-l(or --line)=line (production, staging, dev, int, ...)"
+    echo "\-\-skip skip a backup unit, check https://docs.gitlab.com/ee/raketasks/backup_restore.html#excluding-specific-directories-from-the-backup"
     echo ""
 }
 LINE='prod'
@@ -37,8 +38,11 @@ while [ "$1" != "" ]; do
            SRE_TEAM=$VALUE
            ;;
         -l | --line)
-           LINE=$VALUE
-           ;;
+          LINE=$VALUE
+          ;;
+        --skip)
+          SKIP=$VALUE
+          ;;
         *)
             echo "ERROR: unknown parameter \"$PARAM\""
             usage
@@ -55,8 +59,13 @@ if [ -z "$SRE_TEAM" ]; then
  echo "please define sre team paramete example -s=sret1"
 fi
 
-export POD=$(oc get pod  -o jsonpath='{.items.*.metadata.name}' | sed 's/ /\n/g' | grep 'gitlab-task-runner-') || exit 0 
+export POD=$(oc get pod  -o jsonpath='{.items.*.metadata.name}' | sed 's/ /\n/g' | grep 'gitlab-task-runner-') || exit 0
+if [[ "$SKIP" == "" ]];then
 export OUTPUT=$(oc exec $POD -i "backup-utility")
+else
+echo "SKIPPING $SKIP"
+export OUTPUT=$(oc exec $POD -i "backup-utility SKIP=$SKIP")
+fi
 export RESULT=$?
 if [ -z "$PROMETHEUS_PUSHGATEWAY_URL" ]; then
     echo "$OUTPUT" || exit 0
@@ -66,14 +75,14 @@ else
     export OS=$(oc exec $POD -it "cat" "/etc/os-release" ) || exit 0
     export OS=$(echo "$OS" | grep -e "^NAME=" | sed 's/NAME=//g' | sed 's/"//g')
     export METRICS=$(
-        #define the type of metric 
+        #define the type of metric
         #for every repo we have a metric indicating backup success
         echo "#TYPE gitlab_backup_repo gauge";
-        echo "$OUTPUT" | 
+        echo "$OUTPUT" |
         # awk is your friend to transform the output into prometheus metrics
         awk -v h="$POD" -v ostype="$OS" -v sre_team="$SRE_TEAM"  -v line="$LINE" '/ .*\[.*\]/{
         gsub("Dumping","");
-        gsub("\\\[DONE\\\]",0); 
+        gsub("\\\[DONE\\\]",0);
         gsub("\\\[SKIPPED\\\]",1);
         gsub("\\\[WARNING\\\]",2);
         gsub("\\\[ERROR\\\]",3);
@@ -82,7 +91,7 @@ else
         gsub("/","-");
         gsub("-","_");
         printf "gitlab_backup_repo {repo=\"%s\",certname=\"%s\",os=\"%s\",project=\"gitlab_backup\",line=\"%s\",sre_team=\"%s\"} %d\n",$1,h,ostype,line,sre_team, $NF;
-        }'; 
+        }';
         # finaly the global sucess of the backup
         echo -e "#TYPE gitlab_backup_success gauge"
         echo -e "gitlab_backup_success{certname=\"$H\",os=\"$OS\", project=\"gitlab_backup\",line=\"$LINE\",sre_team=\"$SRE_TEAM\"} $RESULT") || exit 0
@@ -93,5 +102,5 @@ else
     echo -e "output:\n$OUTPUT"
     echo -e "metric:\n$METRICS"
     echo -e "result:\n$RESULT"
-    exit 0 
+    exit 0
 fi
